2caf9f9654f69385dbd647d836916f3e2bae502f
sheikhu <sheikhu02@gmail.com>
2014-07-30 19:29:23 +0000

    first commit

---
+++ app/config/local/app.php
@@ -0,0 +1,28 @@
+<?php
+
+return array(
+
+	/*
+	|--------------------------------------------------------------------------
+	| Application Debug Mode
+	|--------------------------------------------------------------------------
+	|
+	| When your application is in debug mode, detailed error messages with
+	| stack traces will be shown on every error that occurs within your
+	| application. If disabled, a simple generic error page is shown.
+	|
+	*/
+
+	'debug' => true,
+
+	'providers' => append_config(
+		[
+		'Way\Generators\GeneratorsServiceProvider',
+		'Barryvdh\Debugbar\ServiceProvider',
+		]),
+
+	'aliases' => append_config([
+		 'Debugbar' => 'Barryvdh\Debugbar\Facade'
+		 ])
+
+	);
